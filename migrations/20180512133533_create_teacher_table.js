708657055324b03109d1a1d3806f91a4ce5e45a9
Yihui <yihui@redairship.com>
2018-05-13 10:07:14 +0800

    Update exports.up function for teachers table to insert data

--- migrations/20180512133533_create_teacher_table.js
+++ migrations/20180512133533_create_teacher_table.js
@@ -1,9 +1,17 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('teachers', function(table) {
-      table.increments('id').primary();
-      table.string('email').notNullable();
-  });
+    return Promise.all([
+        knex.schema.createTableIfNotExists('teachers', function(table) {
+            table.increments('id').primary();
+            table.string('email').notNullable();
+        }).then(function() {
+            return knex('teachers').insert([
+                {email: 'teacherken@gmail.com'},
+                {email: 'teacherjoe@gmail.com'},
+                {email: 'teacherdavid@gmail.com'}
+            ]);
+        })
+    ]);
 };
 
 exports.down = function(knex, Promise) {
